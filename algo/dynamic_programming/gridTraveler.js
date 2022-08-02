d6ef06ec24e24f0e68ab3fab53226c4df67e2399
ouardaoui <ayoubouardaoui3@gmail.com>
2022-08-02 23:21:28 +0100

    dynamic grid traver

--- algo/dynamic_programming/gridTraveler.js
+++ algo/dynamic_programming/gridTraveler.js
@@ -1,9 +1,12 @@
-const gridTraver = (n,m) => {
+const gridTraver = (n,m,memo = {}) => {
+	const key = n + ',' + m;
+	if(key in memo) return memo[key];
 	if(n === 1 && m === 1)
 		return 1; 
 	if(n === 0 || m === 0)
 		return 0; 
-	return (gridTraver(n - 1, m) + gridTraver(n,m -1));
+	memo[key] = (gridTraver(n - 1, m,memo) + gridTraver(n,m -1,memo));
+	return memo[key];
 }
 
-console.log(gridTraver(3,3))
+console.log(gridTraver(18,18))
